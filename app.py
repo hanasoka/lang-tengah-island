94235d10ff25f647700b9b846370e2aa237009c9
Skylar Asoka <57451015+hanasoka@users.noreply.github.com>
2024-02-26 14:41:04 +0000

    added tour items

--- app.py
+++ app.py
@@ -5,32 +5,54 @@ app = Flask(__name__)
 TOURS = [
   {
     "id": 1,
-    "title": "Day Trip",
-    "description": "A day trip to Lang Tengah Island",
-    "price": "MYR 500.00"
+    "title": "Lang Tengah Day Trip",
+    "description": """
+Experience the wonders of Lang Tengah Island on a thrilling day excursion, offering breathtaking views and unforgettable moments.""",
+    "price": "MYR 250.00"
   },
-
   {
     "id": 2,
-    "title": "Family Picnic",
-    "description": "A beach picnic at Turtle Bay",
-    "price": "MYR 1,000.00"
+    "title": "Lang Tengah 3D2N",
+    "description": """
+Immerse yourself in the natural beauty of Lang Tengah Island during a comprehensive 
+3-day, 2-night adventure, creating memories that last a lifetime.""",
+    "price": "MYR 700.00 - MYR 2500.00"
   },
-
   {
     "id": 3,
-    "title": "Group Trip",
-    "description": "Explore Lang Tengah Island",
-    "price": "MYR 5,000.00"
+    "title": "Lang Tengah Camping",
+    "description": """
+Embark on an adventurous camping journey on Lang Tengah Island, uncovering its 
+hidden gems and embracing the essence of outdoor living.""",
+    "price": "MYR 500.00"
   },
-
   {
     "id": 4,
-    "title": "Private Trip",
-    "description": "Picnic, Adventure, Exploration",
-
+    "title": "Private Group Trip",
+    "description": """
+Customize your perfect day trip for family, friends, or colleagues, ensuring a 
+tailored and unforgettable experience.""",
+    "price": "MYR 5,000.00"
+  },
+  {
+    "id": 5,
+    "title": "Redang Island 3 Days 2 Nights",
+    "description": """
+Discover the enchanting beauty of Redang Island during a memorable 3-day, 
+2-night getaway, offering a range of experiences for all.""",
+    "price": "MYR 700.00 - MYR 5,000.00"
+  },
+  {
+    "id": 6,
+    "title": "Bidong Island Day Trip",
+    "description": """
+Explore the captivating sights of Pulau Bidong, also known as the Little Saigon 
+of Malaysia, on an enriching day trip filled with cultural immersion and scenic views.""",
+    "price": "MYR 200.00"
   }
 ]
+
+
 
 
 @app.route("/")
