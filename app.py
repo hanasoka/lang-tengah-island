449b71eb8f56c040270a2dded7c8c576c597d031
Skylar Asoka <57451015+hanasoka@users.noreply.github.com>
2024-02-29 13:56:34 +0000

    change json data

--- app.py
+++ app.py
@@ -5,63 +5,37 @@ app = Flask(__name__)
 TOURS = [
   {
     "id": 1,
-    "title": "Lang Tengah Day Trip",
-    "description": """
-Experience the wonders of Lang Tengah Island on a thrilling day excursion, offering breathtaking views and unforgettable moments.""",
+    "title": "Day Trip",
+    "description": "Thrilling Lang Tengah Island day trip: explore wonders in a day.",
     "price": "MYR 250.00",
     "image":"/static/turtle.jpg",
     "alt": "Lang Tengah Island"
   },
   {
     "id": 2,
-    "title": "Lang Tengah 3D2N",
-    "description": """
-Immerse yourself in the natural beauty of Lang Tengah Island during a comprehensive 
-3-day, 2-night adventure, creating memories that last a lifetime.""",
-    "price": "MYR 700.00 - MYR 2500.00",
+    "title": "Boat Ticket",
+    "description": "Return public speedboat transfer for easy Lang Tengah access.",
+    "price": "MYR 160.00",
     "image":"/static/turtle.jpg",
     "alt": "Pulau Lang Tengah"
   },
   {
     "id": 3,
-    "title": "Lang Tengah Camping",
-    "description": """
-Embark on an adventurous camping journey on Lang Tengah Island, uncovering its 
-hidden gems and embracing the essence of outdoor living.""",
-    "price": "MYR 500.00",
+    "title": "Boat Charter",
+    "description": "Lang Tengah Island private boat charter for exclusive transfers.",
+    "price": "MYR 600.00",
     "image":"/static/turtle.jpg",
     "alt": "Lang Tengah Campsite"
   },
   {
     "id": 4,
-    "title": "Private Group Trip",
-    "description": """
-Customize your perfect day trip for family, friends, or colleagues, ensuring a 
-tailored and unforgettable experience.""",
+    "title": "Private Day Trip",
+    "description": "Tailored Lang Tengah day trip for family, friends, or colleagues.",
     "price": "MYR 5,000.00",
     "image":"/static/turtle.jpg",
     "alt": "Han at Traisea"
   },
-  {
-    "id": 5,
-    "title": "Redang Island 3 Days 2 Nights",
-    "description": """
-Discover the enchanting beauty of Redang Island during a memorable 3-day, 
-2-night getaway, offering a range of experiences for all.""",
-    "price": "MYR 700.00 - MYR 5,000.00",
-    "image":"/static/turtle.jpg",
-    "alt": "Redang Island"
-  },
-  {
-    "id": 6,
-    "title": "Bidong Island Day Trip",
-    "description": """
-Explore the captivating sights of Pulau Bidong, also known as the Little Saigon 
-of Malaysia, on an enriching day trip filled with cultural immersion and scenic views.""",
-    "price": "MYR 200.00",
-    "image":"/static/banner.jpg",
-    "alt": "Bidong Island"
-  }
+
 ]
 
 
